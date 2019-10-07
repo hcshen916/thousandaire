a5a165861bf2da280be36fa2d49053b6e9b23018
ioumu8 <45913756+ioumu8@users.noreply.github.com>
2019-10-07 15:46:14 +0800

    Set alpha into simulator
    Try to write a simple alpha and put it into simulator to check whether simulator is good or not.
    Functions may work good, but it seem that I should modify the struct more clearly

--- asset_allocation.py
+++ asset_allocation.py
@@ -1,57 +1,10 @@
 import pickle
 import requests
 import xml.etree.ElementTree as ET
-
-"""
-line 9 to line 51 just for using currency data
-"""
-
-class currency_data:
-	def __init__(self, currency_name = None, renew_date = None, history = list()):
-		self.currency_name = currency_name
-		self.history = list(history)
-		self.renew_date = renew_date
-
-	def get_table(self, url):
-		response = requests.get(url)
-		text = response.content.decode('utf-8')
-		start = text.find('<table')
-		end = text.find('</table>') + 8
-		return text[start : end]
-
-	def get_history(self, url, renew_date):
-		counter = 1
-		#self.history = list()
-		ok = True
-		while ok:
-			table = self.get_table(url + '%s&page=%d' %(self.currency_name ,counter))
-			tree = ET.fromstring(table)
-			rows = list(tree[0][1:])
-			if not rows:
-				break
-			for row in rows:
-				grids = list(row)
-				date = grids[0][0].text
-				buy = float(grids[3].text)
-				sell = float(grids[4].text)
-				if(date == renew_date):
-					ok = False
-					break
-				self.history.append((date, buy, sell))
-			counter += 1
-	
-	def upload(self, new_history):
-		self.history = new_history.history + self.history
-
-	def renew(self):
-		print('now ', self.history[0][0])
-		self.renew_date = self.history[0][0]
-
-	def call_data(self):
-		return self.history
+from Crawler import currency_data
 
 class asset_allocation:
-	def __init__(self, date , country = list(), currency_set = {}):
+	def __init__(self, date = None, country = list(), currency_set = {}):
 		self.date = date
 		self.country = country
 		self.currency_set = currency_set
@@ -59,15 +12,68 @@ class asset_allocation:
 		currency_set means how many % for every currency
 		"""
 
-class pnl():
-	def __init__(self, asset, country, base):
+def call_alpha(start, end, now_Data, country_list):
+	return_set = list()
+	for x in range(start, end):
+		today_set = list()
+		for country in country_list:
+			change = now_Data[country].history[x - 1][1] - now_Data[country].history[x - 2][1]
+			change /= now_Data[country].history[x - 2][1]
+			if(change < 0):
+				today_set.append(-change)
+			else:
+				today_set.append(0.0)
+		sum = 0
+		for num in today_set:
+			sum += num
+		if(sum != 0):
+			for num in range(0, len(today_set)):
+				today_set[num] /= sum
+		return_set.append(asset_allocation(now_Data[country].history[x][0], country_list, today_set))
+	return return_set
+
+class simulator():
+	def __init__(self, country, base, asset = list()):
 		self.asset = list(asset)
 		self.country = list(country)
 		self.base = base
-		Data = open("Currency.txt", "rb")
-		now_Data = pickle.load(Data)
-		Data.close()
 		self.value = list()
+		self.day_return = list()
+		Data = open("Currency.txt", "rb")
+		self.now_Data = pickle.load(Data)
+		Data.close()
+
+	def back_test(self, start, end):
+		self.asset += call_alpha(start, end + 1, self.now_Data, self.country)
+		# call_alpha will return an asset_allocation list
+
+	def pnl(self):
+		start = 0
+		for x in range(0, len(self.now_Data['USD'].history)):
+			if(self.now_Data['USD'].history[x][0] == self.asset[0].date):
+				start = x
+				break
+		for x in range(0, len(self.asset)):
+			now_date = self.asset[x].date
+			now_value = 0
+			if(x == 0):
+				now_money = self.base
+			else:
+				now_money = self.value[x - 1]
+			for y in range(0, len(self.country)):
+				invest = now_money * self.asset[x].currency_set[y]
+				now_value += float(invest * self.now_Data[self.country[y]].history[start][1] 
+					                      / self.now_Data[self.country[y]].history[start][2])
+				"""
+					history[date][0] means date, [1] bank buy price, [2] bank sell price
+				"""
+			start += 1
+			self.value.append(now_value)
+		for x in range(0, len(self.asset)):
+			self.day_return.append(float((self.value[x] - self.base) / self.base))
+
+	def update(self, asset):
+		self.asset += asset
 		start = 0
 		for x in range(0, len(now_Data['USD'].history)):
 			if(now_Data['USD'].history[x][0] == asset[0].date):
@@ -76,7 +82,7 @@ class pnl():
 		for x in range(0, len(asset)):
 			now_date = asset[x].date
 			now_value = 0
-			if(x == 0):
+			if(len(self.value) == 0):
 				now_money = self.base
 			else:
 				now_money = self.value[x - 1]
@@ -88,28 +94,18 @@ class pnl():
 				"""
 			start += 1
 			self.value.append(now_value)
-		self.day_return = list()
-		for x in range(0, len(asset)):
+			start = len(self.day_return)
+		for x in range(start, len(self.asset)):
 			self.day_return.append(float((self.value[x] - base) / base))
-
-	def print_data(self, n):
-		"""
-		find the first n days' data 
-		"""
-		length = min(n, len(asset))
+	
+	def print_data(self):
+		length = len(self.asset)
 		for x in range(0, length):
 			print(self.asset[x].date, ' ', self.value[x], ' ', self.day_return[x])
 
-
-asset = list()
-country = ['USD', 'JPY', 'AUD', 'SEK']
-currency_set = [0.2, 0.2, 0.2, 0.4];
-asset.append(asset_allocation('2019-09-26', country, currency_set))
-currency_set = [0.2, 0.3, 0.2, 0.3];
-asset.append(asset_allocation('2019-09-27', country, currency_set))
-currency_set = [0.5, 0.2, 0.2, 0.1];
-asset.append(asset_allocation('2019-09-30', country, currency_set))
-currency_set = [0.6, 0.2, 0.2, 0.0];
-asset.append(asset_allocation('2019-10-01', country, currency_set))
-profit = pnl(asset, country, 10000)
-profit.print_data(10)
+if __name__ == '__main__':
+	country_list = ['USD', 'JPY', 'AUD', 'SEK', 'NZD', 'EUR', 'ZAR', 'GBP']
+	future = simulator(country_list, 10000)
+	future.back_test(0, 50)
+	future.pnl()
+	future.print_data()
